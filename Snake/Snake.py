c68e7295326190b1c1c31ba3d47bdb7213de241a
Даниил <@WIN-ENF4NR8ET31>
2022-03-28 09:05:54 +0300

    Добавьте файлы проекта.

---
+++ Snake/Snake.py
@@ -0,0 +1,26 @@
+
+arr=[]
+for i in range(10):
+    for k in range(10):
+        arr.append('')
+
+class Point:
+     def __init__(self,x,y,color,direction):
+         self.x=x
+         self.y=y
+         self.color=color
+         self.direction=direction
+         
+class Setka:
+
+    def __init__(self,dimesion_):
+        self.Cells=[]
+        self.dimesion=dimesion_
+    def SetPoint(self,point):    
+        self.Cells[point.x][point.y]=point
+        
+
+point=Point(5,6,'Blue','Right')
+setka=Setka(arr)
+setka.SetPoint(point)
+print(setka)
